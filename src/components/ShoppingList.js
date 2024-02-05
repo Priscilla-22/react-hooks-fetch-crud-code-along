49f3e04280071d08abfc13e3bed3f1d05d97312d
Priscilla-22 <priscillamw123@gmail.com>
2024-02-05 13:56:50 +0300

    feat: submit form, a new item will be added to the database and client-side state,

--- src/components/ShoppingList.js
+++ src/components/ShoppingList.js
@@ -1,30 +1,42 @@
-import React, { useState } from "react";
-import ItemForm from "./ItemForm";
-import Filter from "./Filter";
-import Item from "./Item";
+import React, { useState, useEffect } from 'react';
+import ItemForm from './ItemForm';
+import Filter from './Filter';
+import Item from './Item';
 
 function ShoppingList() {
-  const [selectedCategory, setSelectedCategory] = useState("All");
+  const [selectedCategory, setSelectedCategory] = useState('All');
   const [items, setItems] = useState([]);
+
+
+  useEffect(() => {
+    fetch('http://localhost:4000/items')
+      .then((r) => r.json())
+      .then((items) => setItems(items));
+  }, []);
+
+ 
+function handleAddItem(newItem) {
+  setItems([...items, newItem]);
+}
 
   function handleCategoryChange(category) {
     setSelectedCategory(category);
   }
 
   const itemsToDisplay = items.filter((item) => {
-    if (selectedCategory === "All") return true;
+    if (selectedCategory === 'All') return true;
 
     return item.category === selectedCategory;
   });
 
   return (
-    <div className="ShoppingList">
-      <ItemForm />
+    <div className='ShoppingList'>
+      <ItemForm onAddItem={handleAddItem} />
       <Filter
         category={selectedCategory}
         onCategoryChange={handleCategoryChange}
       />
-      <ul className="Items">
+      <ul className='Items'>
         {itemsToDisplay.map((item) => (
           <Item key={item.id} item={item} />
         ))}
